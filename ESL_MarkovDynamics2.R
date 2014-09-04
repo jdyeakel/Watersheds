f1df76689af844fb0d60473747827d111466f79a
jdyeakel <jdyeakel@gmail.com>
2014-09-04 11:42:31 -0600

    Added: calculate difference between p.single=0 and 1 across rescue effects

--- ESL_MarkovDynamics2.R
+++ ESL_MarkovDynamics2.R
@@ -4,6 +4,7 @@ library(RColorBrewer)
 library(igraph)
 library(intergraph)
 library(Rcpp)
+sourceCpp('src/ESL_meta2.cpp')
 
 t.term <- 1000
 n <- 100
@@ -608,6 +609,7 @@ dev.off()
 
 
 #Plotting the Graphs
+colors <- brewer.pal(3,"Set1")
 op <- par(mfrow = c(2,3),
           oma = c(5,4,0,0) + 0.1,
           mar = c(0,3,1,1) + 0.1,
@@ -623,38 +625,274 @@ par(op)
 load("/Users/justinyeakel/Dropbox/Postdoc/2014_Empirical_Watersheds/Optimal_Channel_Nets/Results/ESLdir_m0.1.RData")
 pdf_filename <- paste("/Users/justinyeakel/Dropbox/Postdoc/2014_Empirical_Watersheds/Optimal_Channel_Nets/Results/ESLdir_m",m,".pdf",sep="")
 
-colors <- brewer.pal(3,"Set1"); trans <- ""
-dev.off()
-pdf(file=pdf_filename
-    ,width=3.5,height=6)
-op <- par(mfrow = c(3,1),
-          oma = c(5,4,0,0) + 0.1,
-          mar = c(0,3,1,1) + 0.1,
-          mgp = c(2, 1, 0))
+
+
+
+
+
+############################################################################################
+#Determine the DIFFERENCE between p.single=1 and p.single=0 across ranges of e/c and rescue
+############################################################################################
+
+
+
+
+#Build nondirected (p=0) and directed graphs (p=1) to start
+#Length of simulation
+t.term <- 5000
+#Number of nodes
+n <- 100
+#Number of links
+l <- 200
+repetitions <- 10
+p.single <- 1
+
+graphs <- list()
+dir.graphs <- list()
+
+# #Build initial nets first
+# #Full
+# full.adj.m <- matrix(1,n,n); diag(full.adj.m) <- 0
+# full.net <- graph.adjacency(full.adj.m)
+# graphs[[1]] <- full.net
+
+#Lattice
+lattice.net <- graph.lattice(dimvector=c(sqrt(n),sqrt(n)),directed=TRUE,mutual=TRUE)
+graphs[[1]] <- lattice.net
+edgel <- get.edgelist(lattice.net)
+edgel.new <- edgel
+skip <- 0
+tic <- 0
+del.link <- numeric()
+for (k in 1:length(edgel[,1])) {
+  if ((k %in% skip) == FALSE) {
+    link <- edgel[k,]
+    pos1 <- which(apply(edgel,1,function(x){(x[1]==link[1] && x[2]==link[2])})==TRUE)
+    pos2 <- which(apply(edgel,1,function(x){(x[1]==link[2] && x[2]==link[1])})==TRUE)
+    skip <- c(skip,pos1,pos2)
+    draw.single <- runif(1)
+    if (draw.single < p.single) {
+      tic <- tic + 1
+      del.link[tic] <- sample(c(pos1,pos2),1)
+    }
+  }
+}
+if (length(del.link > 0)) {
+  edgel.new <- edgel.new[-del.link,]
+}
+lattice.net.dir <- graph.edgelist(edgel.new)
+adj.m <- get.adjacency(lattice.net.dir)
+#isSymmetric(as.matrix(adj.m))
+dir.graphs[[1]] <- lattice.net.dir
+
+#Random net
+random.net <- erdos.renyi.game(n,l,type="gnm",directed=FALSE,loops=FALSE)
+nc <- no.clusters(random.net)
+while (nc > 1) {
+  random.net <- erdos.renyi.game(n,l,type="gnm",directed=FALSE,loops=FALSE)
+  nc <- no.clusters(random.net)
+}
+graphs[[2]] <- random.net
+edgelist_in <- get.edgelist(random.net)
+edgelist_out <- matrix(0,length(edgelist_in[,1]),2)
+edgelist_out[,1] <- edgelist_in[,2]; edgelist_out[,2] <- edgelist_in[,1]
+edgelist_dir <- rbind(edgelist_in,edgelist_out)
+random.net.dir <- graph.edgelist(edgelist_dir,directed=TRUE) #FULLY BIDIRECTIONAL GRAPH
+edgel <- get.edgelist(random.net.dir)
+edgel.new <- edgel
+skip <- 0
+tic <- 0
+del.link <- numeric()
+for (k in 1:length(edgel[,1])) {
+  if ((k %in% skip) == FALSE) {
+    link <- edgel[k,]
+    pos1 <- which(apply(edgel,1,function(x){(x[1]==link[1] && x[2]==link[2])})==TRUE)
+    pos2 <- which(apply(edgel,1,function(x){(x[1]==link[2] && x[2]==link[1])})==TRUE)
+    skip <- c(skip,pos1,pos2)
+    draw.single <- runif(1)
+    if (draw.single < p.single) {
+      tic <- tic + 1
+      del.link[tic] <- sample(c(pos1,pos2),1)
+    }
+  }
+}
+if (length(del.link > 0)) {
+  edgel.new <- edgel.new[-del.link,]
+}
+random.net.dir <- graph.edgelist(edgel.new)
+adj.m <- get.adjacency(random.net.dir)
+#isSymmetric(as.matrix(adj.m))
+dir.graphs[[2]] <- random.net.dir
+
+#Scale Free
+#Scale-Free ::
+degs <- sample(1:n, n, replace=TRUE, prob=(1:n)^-1.8)
+edgec <- 1
+while (edgec != 2*l){
+  degs <- sample(1:n, n, replace=TRUE, prob=(1:n)^-1.8)
+  edgec <- sum(degs)
+}
+if (sum(degs) %% 2 != 0) { degs[1] <- degs[1] + 1 }
+sf.net <- degree.sequence.game(out.deg=degs, method="vl")
+graphs[[3]] <- sf.net
+edgelist_in <- get.edgelist(sf.net)
+edgelist_out <- matrix(0,length(edgelist_in[,1]),2)
+edgelist_out[,1] <- edgelist_in[,2]; edgelist_out[,2] <- edgelist_in[,1]
+edgelist_dir <- rbind(edgelist_in,edgelist_out)
+sf.net.dir <- graph.edgelist(edgelist_dir,directed=TRUE) #FULLY BIDIRECTIONAL GRAPH
+#all(degree(sf.net) == degs)
+#adj.m <- get.adjacency(sf.net)
+#isSymmetric(adj.m)
+edgel <- get.edgelist(sf.net.dir)
+edgel.new <- edgel
+skip <- 0
+tic <- 0
+del.link <- numeric()
+for (k in 1:length(edgel[,1])) {
+  if ((k %in% skip) == FALSE) {
+    link <- edgel[k,]
+    pos1 <- which(apply(edgel,1,function(x){(x[1]==link[1] && x[2]==link[2])})==TRUE)
+    pos2 <- which(apply(edgel,1,function(x){(x[1]==link[2] && x[2]==link[1])})==TRUE)
+    skip <- c(skip,pos1,pos2)
+    draw.single <- runif(1)
+    if (draw.single < p.single) {
+      tic <- tic + 1
+      del.link[tic] <- sample(c(pos1,pos2),1)
+    }
+  }
+}
+if (length(del.link > 0)) {
+  edgel.new <- edgel.new[-del.link,]
+}
+sf.net.dir <- graph.edgelist(edgel.new)
+adj.m <- get.adjacency(sf.net.dir)
+dir.graphs[[3]] <- sf.net.dir
+
+MRG <- list()
+MRG.dir <- list()
+
+#Loop through Graphs
 for (i in 1:3) {
   
-  m.e <- MG[[i]][[1]]
-  m.s <- MG[[i]][[2]]
-  m.l <- MG[[i]][[3]]
-  if (i == 3) {
-    plot(ext.seq/c,apply(m.e,1,mean),type="l",col=colors[1],xlim=c(0,2),ylim=c(0,1),xlab="e/c",ylab="Proportion")
-  } else {plot(ext.seq/c,apply(m.e,1,mean),type="l",col=colors[1],xlim=c(0,1.5),ylim=c(0,1), xaxt='n', ann=FALSE)}
-  polygon(x=c(ext.seq/c,rev(ext.seq/c)),
-          y=c(apply(m.e,1,mean)+apply(m.e,1,sd),rev(apply(m.e,1,mean)-apply(m.e,1,sd))),col=paste(colors[1],"50",sep=""),border=NA)
-  lines(ext.seq/c,apply(m.s,1,mean),col=colors[2])
-  polygon(x=c(ext.seq/c,rev(ext.seq/c)),
-          y=c(apply(m.s,1,mean)+apply(m.s,1,sd),rev(apply(m.s,1,mean)-apply(m.s,1,sd))),col=paste(colors[2],"50",sep=""),border=NA)
-  lines(ext.seq/c,apply(m.l,1,mean),col=colors[3])
-  polygon(x=c(ext.seq/c,rev(ext.seq/c)),
-          y=c(apply(m.l,1,mean)+apply(m.l,1,sd),rev(apply(m.l,1,mean)-apply(m.l,1,sd))),col=paste(colors[3],"50",sep=""),border=NA)
-  lines(ext.seq/c,apply(m.s,1,mean)+apply(m.l,1,mean),col="black",lty=2)
-}
-par(op)
-dev.off()
+  MR <- list()
+  MR.dir <- list()
+  
+  #Loop through Rescue Effects
+  m.seq <- seq(0,1,0.1)
+  for (resc in 1:length(m.seq)) {
+    
+    m <- m.seq[resc]
+    
+    print(paste("Graphs = ",i,"/3 :: Rescue Effect = ",m,sep=""))
+    
+    #Establish adjacency matrix
+    if (i==1) {
+      adj.m <- get.adjacency(lattice.net)
+      adj.m.dir <- get.adjacency(lattice.net.dir)
+    }
+    if (i==2) {
+      adj.m <- get.adjacency(random.net)
+      adj.m.dir <- get.adjacency(random.net.dir)
+    }
+    if (i==3) {
+      adj.m <- get.adjacency(sf.net)
+      adj.m.dir <- get.adjacency(sf.net.dir)
+    }
+    
+    #Make list of nearest neighbors for each node
+    nn <- list()
+    for (j in 1:n) {
+      arow <- adj.m[j,]
+      nn[[j]] <- which(arow==1)
+    }
+    nn_cpp <- lapply(nn,function(x){x-1})
+    
+    nn.dir <- list()
+    for (j in 1:n) {
+      arow <- adj.m.dir[j,]
+      nn.dir[[j]] <- which(arow==1)
+    }
+    nn_cpp_dir <- lapply(nn.dir,function(x){x-1})
+    
+    #Extinction sequence (prob(ext) for Large patch)
+    ext.seq <- seq(0,0.4,0.02) #Change to seq(0,0.4,0.02) for future runs... or seq(0,0.4,0.01) for the final product
+    
+    m.e <- matrix(0,length(ext.seq),repetitions)
+    m.s <- matrix(0,length(ext.seq),repetitions)
+    m.l <- matrix(0,length(ext.seq),repetitions)
+    
+    m.e.dir <- matrix(0,length(ext.seq),repetitions)
+    m.s.dir <- matrix(0,length(ext.seq),repetitions)
+    m.l.dir <- matrix(0,length(ext.seq),repetitions)
+    
+    #Iterate over different values of large-patch extinction probabilities
+    #NOTE :: These iterations are now all done in C++
+    
+    #for (j in 1:length(ext.seq)) {
+    #for (rep in 1:replicates) {
+    
+    #Define intial conditions
+    X <- matrix(0,n,t.term)
+    Meta <- matrix(0,3,t.term)
+    #Initiate starting vector
+    initial <- c(n - 2*round(n/3,0),round(n/3,0),round(n/3,0))
+    X[,1] <- c(rep(0,initial[1]),rep(1,initial[2]),rep(2,initial[3]))
+    Meta[,1] <- initial
+    
+    #Values of constants
+    #Probabilities
+    c <- 0.2
+    r <- 0.2
+    #el <- ext.seq[j]
+    #es <- 2*el
+    
+    #Perform Simulation
+    ESL.out <- ESL_meta2(n, t.term, repetitions, X, Meta, nn_cpp, c, r, m, ext.seq)
+    ESL.out.dir <- ESL_meta2(n, t.term, repetitions, X, Meta, nn_cpp_dir, c, r, m, ext.seq)
+    
+    #Save summary statistics
+    #These are matrices of mean proportions with ext.seq=rows and repetitions=columns...
+    for (j in 1:length(ext.seq)) {
+      m.e[j,] <- ESL.out[[1]][[j]][1,]/n
+      m.s[j,] <- ESL.out[[1]][[j]][2,]/n
+      m.l[j,] <- ESL.out[[1]][[j]][3,]/n
+    } 
+    for (j in 1:length(ext.seq)) {
+      m.e.dir[j,] <- ESL.out.dir[[1]][[j]][1,]/n
+      m.s.dir[j,] <- ESL.out.dir[[1]][[j]][2,]/n
+      m.l.dir[j,] <- ESL.out.dir[[1]][[j]][3,]/n
+    } 
+    
+    #} #end r
+    #} #end j
+    
+    M <- list()
+    M.dir <- list()
+    
+    M[[1]] <- m.e
+    M[[2]] <- m.s
+    M[[3]] <- m.l
+    
+    M.dir[[1]] <- m.e.dir
+    M.dir[[2]] <- m.s.dir
+    M.dir[[3]] <- m.l.dir
+    
+    MR[[resc]] <- M
+    
+    MR.dir[[resc]] <- M
+    
+  } # end resc
+  
+  MRG[[i]] <- MR
+  MRG.dir[[i]] <- MR
+  
+} # end i
 
-#Plotting the Graphs
-plot(lattice.net,vertex.size=degree(lattice.net),vertex.label=NA,vertex.color=colors[2],edge.color="lightblue")
-plot(sf.net,vertex.size=degree(sf.net)+2,vertex.label=NA,vertex.color=colors[3],edge.color="lightblue",edge.width=2)
+
+
+
+
+
 
 
 
